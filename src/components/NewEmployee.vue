1a2bf1162da4418497114ef6086bf8ccf2c73cc1
apramodya <pramodyaabeysinghe@gmail.com>
2018-10-14 02:20:07 +0530

    create add employee form

--- src/components/NewEmployee.vue
+++ src/components/NewEmployee.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="new-employee">
     <h3>New employee</h3>
-
+    <div class="row">
+      <form @submit.prevent="saveEmployee" class="col s12">
+        <div class="input-field col s12">
+          <input type="text" v-model="employee_id" id="employee_id">
+          <label for="employee_id">Employee Id</label>
+        </div>
+        <div class="input-field col s12">
+          <input type="text" v-model="name" id="name">
+          <label for="name">Employee Name</label>
+        </div>
+        <div class="input-field col s12">
+          <input type="text" v-model="position" id="position">
+          <label for="position">Employee Position</label>
+        </div>
+        <div class="input-field col s12">
+          <input type="text" v-model="dept" id="dept">
+          <label for="dept">Employee Dept</label>
+        </div>
+        <button class="btn" type="submit">Submit</button>
+        <router-link class="btn grey" to="/">Cancel</router-link>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -12,6 +33,9 @@
       return {
 
       }
+    },
+    methods: {
+      saveEmployee(){}
     }
   }
 </script>
