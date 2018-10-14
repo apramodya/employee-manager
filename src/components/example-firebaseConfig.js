1d89770850762248a273f877e925106402107448
apramodya <pramodyaabeysinghe@gmail.com>
2018-10-14 11:28:21 +0530

    add example-firebaseConfig

--- src/components/example-firebaseConfig.js
+++ src/components/example-firebaseConfig.js
@@ -1,5 +1,5 @@
 export default {
-  apiKey: "AIzaSyCAt-c4WhyE6I_dO2U0cKfLzhmBk4gcldw",
+  apiKey: "",
   authDomain: "vuefs-prod-d0f5a.firebaseapp.com",
   databaseURL: "https://vuefs-prod-d0f5a.firebaseio.com",
   projectId: "vuefs-prod-d0f5a",
