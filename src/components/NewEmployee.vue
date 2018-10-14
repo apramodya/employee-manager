60b37f0438a1e284b5ea9a20282a3503daa8d626
apramodya <pramodyaabeysinghe@gmail.com>
2018-10-14 09:44:39 +0530

    add new employee

--- src/components/NewEmployee.vue
+++ src/components/NewEmployee.vue
@@ -27,15 +27,34 @@
 </template>
 
 <script>
+  import db from './firebaseInit'
+
   export default {
     name: 'new-employee',
     data() {
       return {
-
+        employee_id: null,
+        name: null,
+        dept: null,
+        position: null
       }
     },
     methods: {
-      saveEmployee(){}
+      saveEmployee() {
+        db.collection('employees')
+          .add({
+            id: this.employee_id,
+            name: this.name,
+            dept: this.dept,
+            position: this.position
+          })
+          .then(docRef => {
+            this.$router.push('/')
+          })
+          .catch(error => {
+            console.log(error)
+          })
+      }
     }
   }
 </script>
